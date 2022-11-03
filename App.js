18d18310765482e25717f85b508923da2c3b2ae7
Brandon Tapia <btapia@ucol.mx>
2022-11-03 07:49:36 -0600

    practica 1

--- App.js
+++ App.js
@@ -1,14 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Switch, TextInput, setTask, task} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component<Props> {
+  
+ 
+  constructor(props){
+    super(props);
+    this.state = {
+      switchValue: false,
+      textValue: "",
+    };
+    
+  }
+  onPressLearnMore = () =>{
+    console.warn('Presionaste el botón')
+  }
+
+  onPressText = () =>{
+    alert(this.state.textValue)
+  }
+
+  onChange = (value) => {
+    console.warn(`El switch cambiará a: ${value}`)
+    this.setState({switchValue: value});
+  }
+
+  onChangeText = (text) => {
+    console.warn(`El texto es: ${text}`)
+    this.setState({textValue: text});
+  }
+
+  render() {
+  // const [input, input1] = useState();
+    return (
+      <View style={styles.container}>
+        <Switch
+          onValueChange = {() => this.onChange(!this.state.switchValue)}
+          value = {this.state.switchValue}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Introduce un texto"
+          // onChangeText={input1}
+          // value={input}
+          onChangeText={this.onChangeText}
+        />
+        <Button
+          onPress={this.onPressText}
+          title="Presiona"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress={this.onPressLearnMore}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+/>
+        <Text>Hello from the other side </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +74,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: .5,
+    padding: 10,
+    width: 200,
+  }
 });
